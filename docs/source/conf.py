b53d5cd9ce96fb550addc8a88ec18fabf9adc9ba
Sijin Zhang <zsjzyhzp@gmail.com>
2022-06-04 18:03:45 +1200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Sijin ML tutorial'
+copyright = '2021, Sijin Zhang'
+author = 'Sijin Zhang'
 
 release = '0.1'
 version = '0.1.0'
